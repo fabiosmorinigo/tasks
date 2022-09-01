b8d5a70b9f45b04ca03a1474c78f7bf61d7de62b
fabiosmorinigo <fabio_sosapy@outlook.com>
2022-09-01 10:48:47 -0400

    background tasklist remove

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import './listStyle.css';
 
-
 const TodoList = (props) => {
-
-
   return (
     <article className={props.total >=1 ? 'todoList--absolute' : 'todoList todoListAdd'}>
       {props.children}
